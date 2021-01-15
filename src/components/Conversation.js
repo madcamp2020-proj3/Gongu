9267479cc6c028e590b5fc9bccbf4d33832a39f3
Jaegoomon <jaegoo1199@gmail.com>
2021-01-15 13:08:18 +0900

    사이드바 완성

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -3,12 +3,16 @@ import { ListGroup } from 'react-bootstrap';
 import { useConversations } from '../contexts/ConversationsProvider';
 
 export default function Conversation() {
-    const { conversations } = useConversations();
+    const { conversations, selectConversationIndex } = useConversations();
 
     return (
         <ListGroup variant="flush">
             {conversations.map((conversation, index) => (
-                <ListGroup.Item key={index}>
+                <ListGroup.Item
+                    key={index}
+                    action
+                    onClick={() => selectConversationIndex(index)}
+                    active={conversation.selected}>
                     {conversation.recipients.map(r => r.name).join(', ')}
                 </ListGroup.Item>
             ))}
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,16 @@
 import { FormCheck } from "react-bootstrap";
 import React from 'react'
 import Sidebar from "./Sidebar";
+import OpenConversation from "./OpenConversation";
+import { useConversations } from "../contexts/ConversationsProvider";
 
 export default function Dashboard({ id }) {
+    const { selectedConversation } = useConversations();
+
     return (
-        <div className="d-flex" style={{ height: '100vh'}}>
+        <div className="d-flex" style={{ height: '100vh' }}>
             <Sidebar id={id} />
+            {selectedConversation && <OpenConversation />}
         </div>
     )
 }
