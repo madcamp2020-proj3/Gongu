7963e64b8d049abb58adf151563a4e6a6dcca04b
Jaegoomon <jaegoo1199@gmail.com>
2021-01-15 15:37:43 +0900

    채팅창 UI 완성

--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -8,15 +8,46 @@ export function useConversations() {
     return useContext(ConversationsContext);
 }
 
-export function ConversationsProvider({ children }) {
-    const [conversations, setconversations] = useLocalStorage('conversations', []);
+export function ConversationsProvider({ id, children }) {
+    const [conversations, setConversations] = useLocalStorage('conversations', []);
     const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
     const { contacts } = useContacts();
 
     function createConversation(recipients) {
-        setconversations(prevConversations => {
-            return [...prevConversations, { recipients, message: [] }];
+        setConversations(prevConversations => {
+            return [...prevConversations, { recipients, messages: [] }];
         })
+    }
+
+    function addMessageToConversation({ recipients, text, sender }) {
+        setConversations(prevConversations => {
+            let madeChange = false;
+            const newMessage = { sender, text };
+            const newConversations = prevConversations.map
+                (conversation => {
+                    if (arrayEquality(conversation.recipients, recipients)) {
+                        madeChange = true;
+                        return {
+                            ...conversation,
+                            messages: [...conversation.messages, newMessage]
+                        }
+                    }
+                    return conversation;
+                });
+
+            if (madeChange) {
+                return newConversations;
+            } else {
+                return [
+                    ...prevConversations,
+                    { recipients, messages: [newMessage] }
+                ];
+            }
+        });
+    }
+
+    function sendMessage(recipients, text) {
+        addMessageToConversation({ recipients, text, sender: id })
     }
 
     const formattedConversations = conversations.map((conversation, index) => {
@@ -27,13 +58,24 @@ export function ConversationsProvider({ children }) {
             const name = (contact && contact.name) || recipient;
             return { id: recipient, name };
         });
+
+        const messages = conversation.messages.map(m => {
+            const contact = contacts.find(contact => {
+                return contact.id === m.sender;
+            });
+            const name = (contact && contact.name) || m.sender;
+            const fromMe = id === m.sender;
+            return { ...m, senderName: name, fromMe };
+        });
+
         const selected = index === selectedConversationIndex;
-        return { ...conversation, recipients, selected };
+        return { ...conversation, messages, recipients, selected };
     });
 
     const value = {
         conversations: formattedConversations,
         selectedConversation: formattedConversations[selectedConversationIndex],
+        sendMessage,
         selectConversationIndex: setSelectedConversationIndex,
         createConversation
     }
@@ -43,4 +85,15 @@ export function ConversationsProvider({ children }) {
             {children}
         </ConversationsContext.Provider>
     )
+}
+
+function arrayEquality(a, b) {
+    if (a.length !== b.length) return false;
+
+    a.sort();
+    b.sort();
+
+    return a.every((element, index) => {
+        return element === b[index];
+    });
 }
